7143e62767d187d18038a06a50d00847519f7204
Petru Birzu <petru.birzu96@gmail.com>
2018-02-11 04:32:49 +0100

    added extra origins definition

--- chat/io.js
+++ chat/io.js
@@ -8,6 +8,12 @@ const initialize = (server) => {
   const io = socketIo(server, {
     origins: 'localhost:* petrub.com:* http://petrub.com:* http://www.petrub.com:*',
   });
+  io.origins([
+    'localhost:*',
+    'petrub.com:*',
+    'http://petrub.com:*',
+    'http://www.petrub.com:*'
+  ]);
 
   io.on("connection", (socket) => {
     connections.push(socket);
